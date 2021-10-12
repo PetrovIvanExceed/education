0ddb5963d7905e5ed9298c1b9e4a07d7749cd644
PetrovIvanExceed <petrov.exceedteam@gmail.com>
2021-10-12 17:01:06 +0300

    education JavaScript Basic search-string

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -1,5 +1,10 @@
 function searchString(obj) {
-  // Напиши свой код здесь
+  return  Object.entries(obj).reduce((acc, cur) => {
+    if (![null, undefined, ''].includes(cur[1])) {
+      acc += `${cur[0]}=${cur[1]}&`;
+    }
+    return acc;
+  }, '?').slice(0, -1);
 }
 
 window.searchString = searchString;
